f273887b44a1ea0cca176996d29ee840a0fed284
Seifi <hamidrezaseifi@gmail.com>
2020-10-22 10:56:46 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_KwceNZpJd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '50cckjrrchks3uip0gj30qbakv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
